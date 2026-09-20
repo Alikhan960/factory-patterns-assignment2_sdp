efe70c3b02f631b4f468fed102509b1c9642afe0
Alikhan960 <254421@astanait.edu.kz>
2026-09-20 13:32:21 +0500

    Add Abstract Factory: Abstract Products (Button, Checkbox)

---
+++ src/main/java/com/astanait/patterns/abstractfactory/Button.java
@@ -0,0 +1,7 @@
+package com.astanait.patterns.abstractfactory;
+
+// Abstract Product
+public interface Button {
+    void paint();
+    void click();
+}
---
+++ src/main/java/com/astanait/patterns/abstractfactory/Checkbox.java
@@ -0,0 +1,7 @@
+package com.astanait.patterns.abstractfactory;
+
+// Abstract Product
+public interface Checkbox {
+    void paint();
+    void toggle();
+}
